21619df94cbced24ac49f8a6b00c1503283d31f5
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-18 11:50:57 +0530

    Add files via upload

---
+++ StarPattern/Demo.java
@@ -0,0 +1,12 @@
+package StarPattern;
+
+public class Demo {
+    public static void main(String[] args) {
+        for(int i = 0; i<=5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
