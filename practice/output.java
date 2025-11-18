14fdd4e3b49a0a34cbd7f96dbac62ff0dcb5476a
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-18 11:04:10 +0530

    Add files via upload

---
+++ practice/output.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class output {
+    public static void main(String[] args) {
+        int a=7, b=3;
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(++a);
+        System.out.println(a++);
+
+        //type casting
+        double x = 10.5;
+        int y = (int) x;
+        System.out.print(x);
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Your Name : ");
+        // String name = sc.nextLine();
+        // System.out.print("Enter Your City : ");
+        // String city = sc.nextLine();
+        // System.out.print("Enter Your Age : ");
+        // int age = sc.nextInt();
+        
+        // System.out.println("My Name is " + name + " and My City is " + city + " ,i'm " + age + " old OK!");
+    }
+}
