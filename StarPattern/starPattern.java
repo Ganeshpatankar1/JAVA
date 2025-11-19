45640e694cc45b27fa6287ff24a5684f2a1a6359
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-19 15:35:54 +0530

    Add files via upload

---
+++ StarPattern/starPattern.java
@@ -0,0 +1,14 @@
+class starPattern{
+    public static void main(String[] args) {
+        // int i,j,k;
+        for(int i=1; i<=5; i++){
+            for(int j=i; j<5; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
