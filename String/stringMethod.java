dfd88bb7a1b19d4105bc86d51aa42adf6a996ddd
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-20 16:07:11 +0530

    Add files via upload

---
+++ String/stringMethod.java
@@ -0,0 +1,11 @@
+class stringMethod {
+    public static void main(String[] args) {
+        String a = "Ganesh_Patankar";
+        int value = a.length();
+        String st = a.toUpperCase();
+        String stn = a.toLowerCase();
+        System.out.println(value);
+        System.out.println(st);
+        System.out.println(stn);
+    }
+}
