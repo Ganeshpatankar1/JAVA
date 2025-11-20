70d396b3464abed22ef082cbb304a356c7da6f19
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-20 12:46:34 +0530

    Add files via upload

---
+++ Array/DemoArr.java
@@ -0,0 +1,13 @@
+public class DemoArr {
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+        arr[2] = 45;
+        System.out.println(arr[2]);
+
+        int[] marks = {34,53,534,53,53};
+        for (int i : marks) {
+            System.out.println(i);
+        }
+        
+    }
+}
