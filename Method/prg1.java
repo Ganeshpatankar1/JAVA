74f0d910c7e368b9a692117702c5f4a82e240874
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-21 16:10:04 +0530

    Add files via upload

---
+++ Method/prg1.java
@@ -0,0 +1,9 @@
+class prg1 {
+    static int add(int a, int b){
+     return a+b;   
+    }
+    public static void main(String[] args) {
+        int result = add(23, 32);   //method call
+        System.out.println("Sum : " + result);
+    }
+}
