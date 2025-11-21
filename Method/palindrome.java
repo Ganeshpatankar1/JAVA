14faceb0a4eb166644439df8be2b9fe5274d7e2f
Ganeshpatankar1 <patankarg41@gmail.com>
2025-11-21 16:21:37 +0530

    Add files via upload

---
+++ Method/palindrome.java
@@ -0,0 +1,23 @@
+package Method;
+class palindrome {
+    static boolean isPalindrome(String s){
+            int i = 0;
+            int j = s.length()-1;
+
+            while(i < j){
+                if(s.charAt(i) != s.charAt(j)) return false;
+                i++;
+                j--;
+
+            }
+            return true;
+
+    }
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Tony";
+        
+        System.out.println(isPalindrome(s1));
+        System.out.println(isPalindrome(s2));
+    }
+}
